bb0390723c007f0245dfba4565b19b2d553a5795
ecwu <bitlabwzh@gmail.com>
2017-10-30 00:35:05 +0800

    code with python

---
+++ python/Lab/Lab2/task1.py
@@ -0,0 +1,9 @@
+def main ():
+    print('Hello')
+    # return 1
+## comment here to get two "hello" and a 1 return
+    print('Hello')
+	# return 0
+## comment here to get one "hello" and a 0 return
+
+main ()
