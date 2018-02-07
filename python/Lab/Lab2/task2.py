9801e286b573de9da1263ec25d1b5aefb74183f1
ecwu <bitlabwzh@gmail.com>
2018-02-07 11:34:29 +0800

    finish using package scanf

--- python/Lab/Lab2/task2.py
+++ python/Lab/Lab2/task2.py
@@ -1,9 +1,4 @@
-from parse import *
+from scanf import *
 if __name__ == '__main__':
-
-    percentage = int(input('Percentage:(%)\n'))
-    n = int(input('Number:\n'))
-    #aw = input()
-    #(percentage, ch1, ch2, n) = [t(s) for t, s in zip((int, str, str, int), raw.split())]
-    #r = parse("{:n}{:1}{:1}{:n}\n", raw)
-    print("%s%% %c%c %s is %f" % (percentage, 'o', 'f', n, n*(percentage/100)))
+    percentage, ch1, ch2, number = scanf("%d%c%c%d", input())
+    print("%s%% %c%c %s is %f" % (percentage, ch1, ch2, number, number*(percentage/100)))
